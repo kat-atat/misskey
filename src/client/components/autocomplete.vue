7a9ea437fbafc0d4cd470e7744e9c8b0d7181f3a
kinarin <64@live.jp>
2021-08-15 07:11:57 +0900

    Revert "2文字以上入力するまではemojiのオートコンプリートは開始されない"
    This reverts commit 13c7c60e64e5f6e45f32569f82bea91f5a8ee7cf.

--- src/client/components/autocomplete.vue
+++ src/client/components/autocomplete.vue
@@ -280,7 +280,14 @@ export default defineComponent({
 					}
 				}
 			} else if (this.type == 'emoji') {
-				if (this.q.length < 2) return this.emojis = [];
+				if (this.q == null || this.q == '') {
+					this.emojis = this.emojiDb.filter(x => x.isCustomEmoji && !x.aliasOf).sort((a, b) => {
+						var textA = a.name.toUpperCase();
+						var textB = b.name.toUpperCase();
+						return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+					});
+					return;
+				}
 
 				const matched = [];
 				const max = 30;
